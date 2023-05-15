ee7257406e82ee176d6758f84e57530b9a0394e4
h035126 <dinesh.selvam@huntington.com>
2023-05-15 17:45:33 -0400

    proj init

---
+++ reactProj/reactprojremote/webpack.config.js
@@ -0,0 +1,65 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    port: 3002,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    hot: true
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts'],
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader',
+              'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'react_app',
+      filename: 'remoteEntry.js',
+      exposes: { './App': './src/App'},
+     shared: {
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+  ],
+};
