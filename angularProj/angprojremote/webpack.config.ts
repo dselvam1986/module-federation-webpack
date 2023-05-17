8539ad42f0357a9b670c656eb21b94b342d69d04
Dinesh Selvam <dineshse1986@gmail.com>
2023-05-17 07:58:13 -0400

    new webpack proj

--- angularProj/angprojremote/webpack.config.ts
+++ angularProj/angprojremote/webpack.config.ts
@@ -1,30 +1,43 @@
 import { container } from 'webpack';
-const deps = require('./package.json').dependencies;
 
 module.exports = {
-    output: {
-      publicPath: 'http://localhost:4202/',
-      uniqueName: 'angRemote',
-      scriptType: 'text/javascript',
-    },
-    optimization: {
-      runtimeChunk: false,
-    },
-    devServer: {
-      port: 4202,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
+  mode: 'development',
+  entry: './src/main.ts',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-      hot: true,
-    },
-    plugins: [
-        new container.ModuleFederationPlugin({
-            name: 'angRemote',
-            library: { type: 'var', name: 'angRemote' },
-            filename: 'remoteEntry.js',
-            exposes: {
-                './AngRemoteModule': './src/app/app.module.ts'
-            }
-        }),
     ],
+  },
+  output: {
+    publicPath: 'http://localhost:4202/',
+    uniqueName: 'angRemote',
+    scriptType: 'text/javascript',
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  devServer: {
+    port: 4202,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    hot: true,
+  },
+  plugins: [
+    new container.ModuleFederationPlugin({
+      name: 'angRemote',
+      library: { type: 'var', name: 'angRemote' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AngRemoteModule': './src/app/app.module.ts',
+      },
+    }),
+  ],
 };
